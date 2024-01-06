8849252bab36778ce143d23512a6d0028711b7ae
Yash Mehta <yashmehta0395@gmail.com>
2024-01-06 18:13:20 +0530

    Homepage Setup completed

---
+++ components/auth/login-button.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { loadGetInitialProps } from "next/dist/shared/lib/utils";
+
+interface LoginButtonProps {
+    children: React.ReactNode;
+    mode: "modal" | "redirect",
+    asChild: "boolean";
+};
+
+export const LoginButton = ({
+    children,
+    mode = "redirect",
+    asChild
+}: LoginButtonProps) => {
+    const router = useRouter();
+    const onClick = () => {
+        router.push("/auth/login")
+    }
+
+    if (mode === "modal") {
+        return(
+            <sapn>
+                TODO: Implement modal
+            </sapn>
+        )
+    }
+    return (
+        <span onClick={onClick} className="cursor-pointer">
+            {children}
+        </span>
+    )
+}
