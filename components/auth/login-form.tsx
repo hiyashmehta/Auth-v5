a0ee88f69706a1d7808d8077946fc5305fc614fd
Yash Mehta <yashmehta0395@gmail.com>
2024-01-12 21:34:45 +0530

    Two factor Authentication implemented to our app

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -31,6 +31,7 @@ const LoginForm = () => {
         ? "Email already in use with different provider!"
         : "";
 
+    const [showTwoFactor, setShowTwoFactor] = useState(false);
     const [error, setError] = useState<string | undefined>("");
     const [success, setSuccess] = useState<string | undefined>("");
     const [isPending, startTransition] = useTransition();
@@ -50,9 +51,21 @@ const onSubmit = (values: z.infer<typeof LoginSchema>) => {
     startTransition(() => {
     login(values)
         .then((data) => {
-            setError(data?.error);
-            setSuccess(data?.success);
+            if (data?.error) {
+                form.reset();
+                setError(data.error);
+            }
+
+            if (data?.success) {
+                form.reset();
+                setSuccess(data.success);
+            }
+
+            if (data?.twoFactor) {
+                setShowTwoFactor(true);
+            }
         })
+            .catch(() => setError("Something went wrong"));
 });
 }
 
@@ -70,6 +83,27 @@ const onSubmit = (values: z.infer<typeof LoginSchema>) => {
                     className="space-y-6"
                 >
                     <div className="space-y-4">
+                        {showTwoFactor && (
+                                     <FormField
+                                     control={form.control}
+                                     name= "code"
+                                     render={({ field }) => (
+                                         <FormItem>
+                                             <FormLabel>Two Factor Code</FormLabel>
+                                             <FormControl>
+                                                 <Input
+                                                     {...field}
+                                                     disabled={isPending}
+                                                     placeholder="123456"
+                                                 />
+                                             </FormControl>
+                                             <FormMessage />
+                                         </FormItem>
+                                     )}
+                                 />
+                        )}
+                        { !showTwoFactor && (
+                            <>
                         <FormField
                             control={form.control}
                             name= "email"
@@ -116,6 +150,8 @@ const onSubmit = (values: z.infer<typeof LoginSchema>) => {
                                 </FormItem>
                             )}
                         />
+                        </>
+                        )}
                     </div>
                     <FormError message={error || urlError} />
                     <FormSuccess message={success} />
@@ -124,7 +160,7 @@ const onSubmit = (values: z.infer<typeof LoginSchema>) => {
                         type="submit"
                         className="w-full"
                     >
-                        Login
+                        {showTwoFactor ? "Confirm" : "Login"}
                     </Button>
                 </form>
             </Form>
